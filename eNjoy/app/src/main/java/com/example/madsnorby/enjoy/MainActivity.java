b2d86f54b64b158ea56ddb76323f286101f1fccd
Xrosby <mads.sfg@gmail.com>
2019-04-16 15:53:44 +0200

    lol

--- eNjoy/app/src/main/java/com/example/madsnorby/enjoy/MainActivity.java
+++ eNjoy/app/src/main/java/com/example/madsnorby/enjoy/MainActivity.java
@@ -1,14 +1,8 @@
 package com.example.madsnorby.enjoy;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
 import android.widget.ListView;
-import android.widget.TextView;
-
-import com.example.madsnorby.enjoy.adapters.ProductAdapter;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -28,8 +22,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        this.initPlaceholderProducts();
+
         ProductAdapter productAdapter = new ProductAdapter(this, this.products);
         productListView = (ListView) findViewById(R.id.productListView);
+
         productListView.setAdapter(productAdapter);
 
 
--- eNjoy/app/src/main/java/com/example/madsnorby/enjoy/adapters/ProductAdapter.java
+++ eNjoy/app/src/main/java/com/example/madsnorby/enjoy/adapters/ProductAdapter.java
@@ -1,50 +1,43 @@
 package com.example.madsnorby.enjoy.adapters;
 
-import android.app.Activity;
-import android.view.LayoutInflater;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import com.example.madsnorby.enjoy.R;
 
 import java.util.ArrayList;
 
 import domain.ProductSpecification;
 
-public class ProductAdapter extends ArrayAdapter {
+public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.MyViewHolder> {
 
-    private final Activity context;
     private ArrayList<ProductSpecification> products;
 
-    public ProductAdapter(Activity context, ArrayList<ProductSpecification> products) {
-
-        super(context, R.layout.item_product);
-
-        this.context = context;
+    public ProductAdapter(ArrayList<ProductSpecification> products) {
         this.products = products;
+    }
+
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = parent
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProductAdapter.MyViewHolder holder, int position) {
 
     }
 
-    public View getView(int position, View view, ViewGroup parent) {
-        LayoutInflater inflater=context.getLayoutInflater();
-        View productView=inflater.inflate(R.layout.item_product, null,true);
+    @Override
+    public int getItemCount() {
+        return products.size();
+    }
 
-        //this code gets references to objects in the listview_row.xml file
-        TextView nameTextField = (TextView) productView.findViewById(R.id.productName);
-        TextView descriptionTextField = (TextView) productView.findViewById(R.id.productDescription);
-        TextView priceField= (TextView) productView.findViewById(R.id.productPrice);
 
-        //this code sets the values of the objects to values from the arrays
-        nameTextField.setText(products.get(position).getName());
-        descriptionTextField.setText(products.get(position).getDescription());
-        priceField.setText(String.valueOf(products.get(position).getPrice()));
-
-        return productView;
-
-    };
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+        public MyViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
 }
-
-
